981fa65363384b60f2c49dd14dac3e472778296d
Kaitlyn Lee <kaitlyn_l@rogers.com>
2019-01-21 16:53:09 -0500

    Update GameApplet.java

--- GameApplet.java
+++ GameApplet.java
@@ -3,114 +3,139 @@
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
 
 public class GameApplet extends Applet implements ActionListener, MouseListener, MouseMotionListener
 {
-    // Place instance variables here
+    //  instance variables 
     Graphics g;
-    Panel foundations = new Panel (); //spaces on the top. aim of game is to fill these.
-    Panel tableau = new Panel (); //centre. cards can be placed here
-    Panel stock = new Panel (); //waste and draw piles
 
-    BorderLayout lm = new BorderLayout ();
+
+    FlowLayout lm = new FlowLayout (FlowLayout.LEFT, 20, 20);
+    int style = Font.BOLD | Font.ITALIC;
+    Font font = new Font ("garamond", style, 100);
 
     boolean OKtoMove = false;
+    boolean OKtoMoveWaste = false;
+
+    Button buttonRestart = new Button ("Restart");
+    Button buttonQuit = new Button ("Quit");
 
     DeckClass foundationD[] = new DeckClass [8];  //array of foundation deck
     DeckClass tableauD[] = new DeckClass [12]; //array of tableau decks
     DeckClass wasteD = new DeckClass ();
-    DeckClass drawD = new DeckClass ('2');
+    DeckClass drawD = new DeckClass (2);
+
+    TextField time = new TextField (10);
+    long startTime;
+    long endTime;
+
+
 
     boolean winFlag = false;
     boolean loseFlag = false;
+
+    boolean wasteClicked = false;
+
     boolean validMove = false;
 
-    DeckClass takenFromDeck = new DeckClass (); //dummy deck class
-    CardClass test = new CardClass ();
 
-    DeckClass draggedCards = new DeckClass ();
+    CardClass dragCard = null;
+    DeckClass dragPile = new DeckClass ();
+    DeckClass takenFromDeck = new DeckClass ();
 
-    CardClass dummy = new CardClass();
 
     public void initializations ()
     {
-	for (int i = 0 ; i < 8 ; i++)
-	{
-	    foundationD [i] = new DeckClass ();
-	}
+        foundationD = new DeckClass [8];  //array of foundation deck
+        tableauD = new DeckClass [12]; //array of tableau decks
+        wasteD = new DeckClass ();
+        drawD = new DeckClass (2);
 
-	for (int i = 0 ; i < 12 ; i++)
-	{
-	    tableauD [i] = new DeckClass ();
-	}
+        dragCard = null;
+        dragPile = new DeckClass ();
+        takenFromDeck = new DeckClass ();
 
-	dummy = new CardClass (1, 2);
-	dummy.setCentre (foundationD[0].iCentreX, foundationD[0].iCentreY);
-	foundationD [0].addCard (dummy);
-	dummy = new CardClass(3,4);
-	dummy.setCentre (foundationD[2].iCentreX, foundationD[2].iCentreY);
-	foundationD [2].addCard (dummy);
+        winFlag = false;
+        loseFlag = false;
+
+        validMove = false;
+
+        drawD.shuffle ();
+        drawD.setCentre (800, 50);
+
+        wasteD.setCentre (900, 50);
+
+        for (int i = 0 ; i < 8 ; i++)
+        {
+            foundationD [i] = new DeckClass ();
+            foundationD [i].setCentre (i * (foundationD [i].iWidth) + 50, 50);
+        }
+
+        for (int i = 0 ; i < 12 ; i++)
+        {
+            tableauD [i] = new DeckClass ();
+            tableauD [i].addCard (drawD.dealCard (0));
+            tableauD [i].setCentre (i * (tableauD [i].iWidth) + 50, 200);
+        }
+
+        //drawD.flipAllCards ();
+        startTime = System.currentTimeMillis ();
+        endTime = 0;
+        
+        System.out.println("You have 5 min to win the game. Cards can be stacked in the tableau, but only on top of cards of the same suit and in ascending order.");
+
     }
 
 
     public void init ()
     {
-	setLayout (lm);
-	//  foundations.setLayout (new FlowLayout (FlowLayout.CENTER, 20, 20));
-	setSize (1200, 600);
-	initializations ();
+        setLayout (lm);
+        setBackground (Color.pink);
+        setSize (1200, 600);
+        initializations ();
 
+        addMouseListener (this);
+        addMouseMotionListener (this);
 
+        add (buttonRestart);
+        add (buttonQuit);
+        add (time);
+        buttonRestart.addActionListener (this);  // this is the button
+        buttonQuit.addActionListener (this);
 
-
-	addMouseListener (this);
-	addMouseMotionListener (this);
-
-
-	// Place the body of the initialization method here
+        // body of the initialization method here
     } // init method
 
 
-    public boolean validMove (DeckClass deck, CardClass card)
+    public void update (Graphics g)  // gets rid of the flickering caused by repainting
     {
-	if (deck.isDeckEmpty ())
-	{
-	    return true;
-	}
-	else if (card.cSuit == deck.topCard ().cSuit && card.faceValue == deck.topCard ().faceValue)
-	{
-	    return true;
-	}
-	else
-	{
-	    return false;
-	}
-    }
+        Image offScreenImage = createImage (getWidth (), getHeight ());
 
+        Graphics offScreenG = offScreenImage.getGraphics ();
 
-    public void chooseCard (DeckClass deck)  //determines which card was clicked on
-    {
-	CardClass temp = new CardClass ();
+        offScreenG.setColor (getBackground ());
+        offScreenG.fillRect (getX (), getY (), getWidth (), getHeight ());
+        offScreenG.setColor (getForeground ());
 
-	if (!deck.isDeckEmpty ())
-	{
-	    temp = deck.dealCard (0);
-	    
-	    draggedCards.addCard (temp);
+        paint (offScreenG);
 
-	    takenFromDeck = deck;
-	}
-    }
-
-
+        g.drawImage (offScreenImage, getX () - 6, getY () - 30, this);
+    } // update method
 
 
     public void actionPerformed (ActionEvent e)
     {
-	Object objSource = e.getSource ();
+        Object objSource = e.getSource ();
 
-
+        if (objSource == buttonQuit)
+        {
+            System.exit (0);
+        }
+        else if (objSource == buttonRestart)
+        {
+            initializations ();
+            repaint ();
+        }
     }
 
 
@@ -129,113 +154,205 @@ public class GameApplet extends Applet implements ActionListener, MouseListener,
     }
 
 
-    // public void moveCard (DeckClass d1)
-    // {
-    //     if (validMove (d1, (CardClass) draggedCards.deck.get (0)))
-    //     {
-    //         d1.deck.add (draggedCards);
-    //
-    //     }
-    //     else
-    //     {
-    //         takenFromDeck.deck.add (draggedCards);
-    //
-    //     }
-    // }
+    public void chooseCard (DeckClass d1)
+    {
+        dragPile.addCard (d1.dealCard (d1.deck.size () - 1));
+        takenFromDeck = d1;
+    }
+
+
+    public boolean validMove (DeckClass d1, char type)  // type of deck that card will be added to
+    {
+
+
+
+        if (type == 'f') //foundation
+        {
+            if (d1.isDeckEmpty ())
+            {
+                if (((CardClass) dragPile.deck.elementAt (0)).faceValue == 'A') // if empty and adding ace
+                {
+                    return true;
+                }
+            }
+            else // not Empty deck
+            {
+                if (d1.lastCard ().faceValueNum + 1 == ((CardClass) dragPile.deck.elementAt (0)).faceValueNum && d1.lastCard ().cSuit == ((CardClass) dragPile.deck.elementAt (0)).cSuit)  // if adding same suit and card that is 1 higher
+                {
+                    return true;
+                }
+                else if (d1.lastCard ().faceValue == 'K' && ((CardClass) dragPile.deck.elementAt (0)).faceValue == 'A' && d1.lastCard ().cSuit == ((CardClass) dragPile.deck.elementAt (0)).cSuit) // add ace to king
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+
+        if (type == 't') //tableau
+        {
+            if (d1.isDeckEmpty ())
+            {
+                return true;
+            }
+            else //deck not empty
+            {
+                if (d1.lastCard ().faceValueNum == ((CardClass) dragPile.deck.elementAt (0)).faceValueNum + 1 && d1.lastCard ().cSuit == ((CardClass) dragPile.deck.elementAt (0)).cSuit)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        return false;
+
+    }
 
 
     public void mousePressed (MouseEvent e)
     {
-    
 
-	for (int i = 0 ; i < 8 ; i++)
-	{
-	    if (foundationD [i].isPointInside (e.getX (), e.getY ()) == true)
-	    {
-		chooseCard (foundationD [i]);
-		OKtoMove = true;
-	    }
-	}
+        if (drawD.isPointInside (e.getX (), e.getY ()))
+        {
+            if (!drawD.isDeckEmpty ())
+            {
+                wasteD.addCard (drawD.dealCard (drawD.deck.size () - 1));
+            }
+            else
+            {
+                int temp = wasteD.deck.size ();
+                for (int i = 0 ; i < temp ; i++)
+                {
+                    drawD.addCard (wasteD.dealCard (wasteD.deck.size () - 1));
+                }
+            }
+        }
 
-	for (int i = 0 ; i < 12 ; i++)
-	{
-	    if (tableauD [i].isPointInside (e.getX (), e.getY ()) == true)
-	    {
-		chooseCard (tableauD [i]);
-		OKtoMove = true;
-	    }
-	}
 
-	// OKtoMove = true;
-	// test.setCentre (e.getX (), e.getY ());
-	repaint ();
+        for (int i = 0 ; i < 8 ; i++) //to check if foundations are clicked
+        {
+            if (foundationD [i].isPointInside (e.getX (), e.getY ()) && !foundationD [i].isDeckEmpty ())
+            {
+                chooseCard (foundationD [i]);
+                OKtoMove = true;
+            }
+        }
+
+
+        for (int i = 0 ; i < 12 ; i++)
+        {
+            int temp = tableauD [i].deck.size (); //tableaus
+
+            for (int j = 0 ; j < temp ; j++)
+            {
+                if (j < tableauD[i].deck.size() && ((CardClass) (tableauD [i].deck.elementAt (j))).isPointInside (e.getX (), e.getY ()) && !tableauD [i].isDeckEmpty ())
+                {
+                    for (int k = j ; k < temp ; k++)
+                    {
+                        dragPile.addCard (tableauD [i].dealCard (j));
+                    }
+                    takenFromDeck = tableauD [i];
+                    OKtoMove = true;
+                }
+            }
+        }
+
+
+        if (wasteD.isPointInside (e.getX (), e.getY ()) && !wasteD.isDeckEmpty ())
+        {
+            chooseCard (wasteD);
+            OKtoMoveWaste = true;
+            wasteClicked = true;
+        }
+
+
+        repaint ();
     }
 
 
     public void mouseReleased (MouseEvent e)
     {
-	OKtoMove = false;
+        if ((!OKtoMove && !OKtoMoveWaste) || dragPile == null || dragPile.isDeckEmpty ())
+        {
+            return;
+        }
 
-	if (draggedCards.isDeckEmpty ())
-	{
-	    return;
-	}
+        OKtoMove = false;
+        OKtoMoveWaste = false;
+
+        boolean moved = false;
 
 
-	CardClass temp = (CardClass) draggedCards.deck.get (0);
-	boolean moved = false;
+        for (int i = 0 ; i < 8 ; i++) //placing on foundation -- not yet checked if valid yet
+        {
+            if (foundationD [i].isPointInside (e.getX (), e.getY ()))
+            {
+                if (validMove (foundationD [i], 'f'))
+                {
+                    foundationD [i].deck.addAll (dragPile.deck);
+                    dragPile = new DeckClass ();
+                    moved = true;
+                }
+            }
+        }
 
-	for (int i = 0 ; i < 8 ; i++)
-	{
-	    if (foundationD [i].isPointInside (e.getX (), e.getY ()))
-	    {
-		if (validMove (foundationD [i], temp))
-		{
-		    foundationD [i].deck.add (draggedCards);
-		    moved = true;
-		}
-	    }
-	}
-	for (int i = 0 ; i < 12 ; i++)
-	{
-	    if (tableauD [i].isPointInside (e.getX (), e.getY ()))
-	    {
-		if (validMove (tableauD [i], temp))
-		{
-		    tableauD [i].deck.add (draggedCards);
-		    moved = true;
-		}
-	    }
-	}
 
-	if (!moved)
-	{
-	    takenFromDeck.addCard (draggedCards);
-	}
-	repaint ();
+        for (int i = 0 ; i < 12 ; i++)
+        {
+            if (tableauD [i].isPointInside (e.getX (), e.getY ()))
+            {
+                if (validMove (tableauD [i], 't'))
+                {
+                    tableauD [i].deck.addAll (dragPile.deck);
+
+                    dragPile = new DeckClass ();
+                    moved = true;
+                }
+            }
+        }
+
+        if (!moved && wasteClicked == false)
+        {
+            takenFromDeck.deck.addAll (dragPile.deck);
+
+            dragPile = new DeckClass ();
+        }
+
+
+
+        if (!moved && wasteClicked == true)
+        {
+            wasteD.addCard (dragPile.dealCard (0));
+            dragPile = new DeckClass ();
+            wasteD.setCentre (900, 50);
+            wasteClicked = false;
+        }
+
+        wasteClicked = false;
+
+        repaint ();
 
     }
 
 
     public void mouseDragged (MouseEvent e)
     {
-	if (OKtoMove == true)
-	{
-	    if (draggedCards.isDeckEmpty ())
-	    {
-		return;
-	    }
+        if (OKtoMove || OKtoMoveWaste)
+        {
+            dragPile.setCentre (e.getX () - (dragPile.iWidth / 2), e.getY () - (dragPile.iHeight / 2));
 
-	    for (int i = 0 ; i < draggedCards.deck.size () ; i++)
-	    {
-		CardClass temp = (CardClass) draggedCards.deck.get (i);
-		temp.setCentre (e.getX (), e.getY ()); //not acctually centre, it's where mouse is
-	    }
-	    //test.setCentre (e.getX () - test.iWidth / 2, e.getY () - test.iHeight / 2);
-	    repaint ();
-
-	}
-
+            repaint ();
+        }
     }
 
 
@@ -244,34 +361,77 @@ public class GameApplet extends Applet implements ActionListener, MouseListener,
     }
 
 
+    public boolean checkWin ()
+    {
+        for (int i = 0 ; i < 8 ; i++)
+        {
+            if (!foundationD [i].isDeckEmpty () && (foundationD [i].lastCard ()).faceValue == 'K')
+            {
+                winFlag = true;
+            }
+            else
+            {
+                winFlag = false;
+                return false;
+            }
+        }
+        return winFlag;
+    }
+
+
+    public boolean checkLose ()
+    {
+        if (((endTime - startTime) / 1000) > 300)
+        {
+            loseFlag = true;
+        }
+        return loseFlag;
+    }
+
+
     public void paint (Graphics g)
     {
 
-	for (int i = 0 ; i < draggedCards.deck.size () ; i++)
-	{
-	    CardClass temp = (CardClass) draggedCards.deck.get (i);
-	    temp.draw (g);
-	}
-
-	for (int i = 0 ; i < 8 ; i++)
-	{
-
-	    foundationD [i].setCentre (test.iWidth * i + 50, 50);
-	    foundationD [i].draw (g);
-	}
-
-	for (int i = 0 ; i < 12 ; i++)
-	{
-	    tableauD [i].setCentre (test.iWidth * i + 50, 200);
-	    tableauD [i].draw (g);
-	}
+        if (dragPile.deck.size () > 0 && (OKtoMove == true || OKtoMoveWaste == true))
+        {
+            dragPile.fanOut ("down", g);
+        }
 
 
+        for (int i = 0 ; i < 8 ; i++)
+        {
+            foundationD [i].draw (g, true);
+        }
 
-	//g.drawImage (test.img, test.iCentreX, test.iCentreY, this);
+
+        for (int i = 0 ; i < 12 ; i++)
+        {
+            tableauD [i].fanOut ("down", g);
+
+        }
 
 
-	// Place the body of the drawing method here
+        drawD.draw (g);
+        wasteD.draw (g);
+
+
+        endTime = System.currentTimeMillis ();
+        time.setText (String.valueOf ((endTime - startTime) / 1000)+ " seconds");
+
+        if (checkWin ())
+        {
+            g.setFont (font);
+            g.drawString ("Winner", 100, 500);
+        }
+
+        if (checkLose ())
+        {
+            g.setFont (font);
+            g.drawString ("Time is up. You lost :(", 50, 500);
+        }
+
+
+        // body of the drawing method here
     } // paint method
 } // GameApplet class
 
